f19c8948f9c8103646c26f5dded2a03188f23806
skaparelos <skaparelos@gmail.com>
2024-06-28 12:44:07 +0300

    changes

---
+++ README.md
@@ -0,0 +1,7 @@
+in `instrumentation.ts` replace:
+`url: 'https://service_url/v1/traces',`
+
+```
+$ npm i
+$ npm run start
+````
--- instrumentation.ts
+++ instrumentation.ts
@@ -2,9 +2,15 @@ import { NodeSDK } from "@opentelemetry/sdk-node";
 import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 import { PeriodicExportingMetricReader, ConsoleMetricExporter } from "@opentelemetry/sdk-metrics";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 
 const sdk = new NodeSDK({
-  traceExporter: new ConsoleSpanExporter(),
+  // traceExporter: new ConsoleSpanExporter(),
+  traceExporter: new OTLPTraceExporter({
+    // url: 'http://localhost:4318/v1/traces',
+    url: 'https://service_url/v1/traces',
+    headers: {}
+  }),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new ConsoleMetricExporter()
   }),
